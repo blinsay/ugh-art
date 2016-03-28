c8fc6cdd6121508d07d24d76b882bd8308ee4b45
Ben Linsay <blinsay@gmail.com>
2016-03-27 21:59:53 -0400

    clj 1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ugh-art "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [quil "2.2.6"]]
   :repl-options {:init-ns ugh-art.repl})
